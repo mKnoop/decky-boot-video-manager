f2c2b6c330f56ef57b957656afb8c99d176f6a92
Martin Knoop <martin.knoop@bestit-online.de>
2022-10-19 21:30:28 +0200

    Add video change function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import asyncio
 
 class Plugin:
     # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
@@ -12,3 +13,14 @@ class Plugin:
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
         pass
+
+    async def setVideo(self, video):
+        video_path = f'/home/deck/.steam/root/config/uioverrides/movies/deck_startup.webm'
+        if os.path.islink(video_path) or os.path.exists(video_path):
+            os.remove(video_path)
+
+        os.symlink(f'/home/deck/Videos/Boot Videos/' + video, video_path)
+
+plugin = Plugin()
+print(asyncio.run(plugin.getVideos()))
+print(asyncio.run(plugin.setVideo('handheld_history_ultimate_edition_home_ui.webm')))
