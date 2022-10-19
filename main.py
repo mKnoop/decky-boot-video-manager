66cb2b6c570cded0c952c2b6d9a9bf7bd5707197
Martin Knoop <martin.knoop@bestit-online.de>
2022-10-19 21:54:24 +0200

    Add hasCurrent video function

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import glob
 import os
 
 class Plugin:
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
     async def getVideos(self):
         videos = []
         for file in list(glob.glob("/home/deck/Videos/Boot Videos/*.webm")):
              videos.append(os.path.basename(file))
         return videos
 
-    # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
+    async def hasCurrentVideo(self):
+       video_path = f'/home/deck/.steam/root/config/uioverrides/movies/deck_startup.webm'
+       return os.path.islink(video_path) or os.path.exists(video_path):
+
     async def _main(self):
         pass
 
